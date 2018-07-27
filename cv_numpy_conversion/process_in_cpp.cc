e974d8293368a87f462ea03e0d126d45c2c550fa
Toraudonn <toraudonn@gmail.com>
2018-07-27 16:25:50 +0900

    single channel to 3 channel

--- cv_numpy_conversion/process_in_cpp.cc
+++ cv_numpy_conversion/process_in_cpp.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #include <pybind11/eigen.h>
 
 #include <Eigen/Core>
@@ -8,30 +9,36 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/eigen.hpp>
 
-Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
-process_single_channel_image(Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> src_img) {
+//Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>
+void process_single_channel_image(std::array<Eigen::Matrix<uint8_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>, 3> src_img) {
     // convert to cv::Mat
-    cv::Mat cv_src_img(src_img.rows(), src_img.cols(), CV_8UC1, src_img.data());
-    
+    cv::Mat rgb[3];
+    cv::Mat cv_src_img(src_img[0].rows(), src_img[0].cols(), CV_8UC3);
+    for (int i=0;i<src_img.size();i++)
+    {
+        cv::Mat channel(src_img[i].rows(), src_img[i].cols(), CV_8UC1, src_img[i].data());
+        rgb[i] = channel;
+    }
+    cv::merge(rgb, 3, cv_src_img);
     cv::imshow("window_cpp_1", cv_src_img);
     cv::waitKey();
 
-    cv::Mat cv_dst_img;
-    cv::rotate(cv_src_img, cv_dst_img, cv::ROTATE_90_CLOCKWISE);
+    // cv::Mat cv_dst_img;
+    // cv::rotate(cv_src_img, cv_dst_img, cv::ROTATE_90_CLOCKWISE);
 
-    cv::imshow("window_cpp_1", cv_dst_img);
-    cv::waitKey();
+    // cv::imshow("window_cpp_1", cv_dst_img);
+    // cv::waitKey();
 
-    // convert to Eigen::Matrix
-    using c1_stride = Eigen::Stride<Eigen::Dynamic, 1>;
-    using Eigen::Dynamic;
-    using Eigen::RowMajor;
-    using Eigen::Unaligned;
-    c1_stride stride(cv_dst_img.cols, 1);
-    auto dst_img = Eigen::Map<Eigen::Matrix<uint8_t, Dynamic, Dynamic, RowMajor>, Unaligned, c1_stride>(
-        reinterpret_cast<uint8_t*>(cv_dst_img.data), cv_dst_img.rows, cv_dst_img.cols, stride);
+    // // convert to Eigen::Matrix
+    // using c1_stride = Eigen::Stride<Eigen::Dynamic, 1>;
+    // using Eigen::Dynamic;
+    // using Eigen::RowMajor;
+    // using Eigen::Unaligned;
+    // c1_stride stride(cv_dst_img.cols, 1);
+    // auto dst_img = Eigen::Map<Eigen::Matrix<uint8_t, Dynamic, Dynamic, RowMajor>, Unaligned, c1_stride>(
+    //     reinterpret_cast<uint8_t*>(cv_dst_img.data), cv_dst_img.rows, cv_dst_img.cols, stride);
 
-    return dst_img;
+    // return dst_img;
 }
 
 PYBIND11_MODULE(process_in_cpp, m) {
--- cv_numpy_conversion/test.py
+++ cv_numpy_conversion/test.py
@@ -1,13 +1,23 @@
 import cv2
 import process_in_cpp
 
-src_img = cv2.imread("./lena_gray.bmp")
-src_img = src_img[:, :, 0]
+src_img = cv2.imread("./pano.jpg")
+src_img = src_img[:, :, :]
+
+print(type(src_img))
+print(src_img.shape)
 
 cv2.imshow("window_python_1", src_img)
 cv2.waitKey()
 
-dst_img = process_in_cpp.process_single_channel_image(src_img)
 
-cv2.imshow("window_python_2", dst_img)
-cv2.waitKey()
+# convert as array[np.array2d]
+
+arr = [src_img[:,:, 0], src_img[:, :, 1], src_img[:, :, 2]]
+
+print(type(arr))
+print(type(arr[0]))
+
+dst_img = process_in_cpp.process_single_channel_image(arr)
+# cv2.imshow("window_python_2", dst_img)
+# cv2.waitKey()
